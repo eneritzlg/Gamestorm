fbcbb60ec6d8577d546cbda6d4e35064df8d054d
ene <eneritz.langoyo@insitutvidreres.cat>
2024-12-13 14:21:51 +0100

    Components varis i correcció de errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 import { FooterComponent} from './footer/footer.component';
+import { ProductosComponent} from './productos/productos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, ProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { PaginaInicioComponent} from './pagina-inicio/pagina-inicio.component'
+import { PaginaInicioComponent} from './pagina-inicio/pagina-inicio.component';
+import {PaginaFAQComponent } from './pagina-faq/pagina-faq.component';
+import { PaginaCondicionesComponent} from './pagina-condiciones/pagina-condiciones.component';
 
+
+// @ts-ignore
+// @ts-ignore
 export const routes: Routes = [
-  {path: '', component: PaginaInicioComponent}
+  {path: '', component: PaginaInicioComponent, PaginaFAQComponent, PaginaCondicionesComponent}
 ];
