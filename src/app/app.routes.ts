b51b0cbd2558bc94d0b7b1414eaca10b9bb8e144
ene <eneritz.langoyo@insitutvidreres.cat>
2024-12-13 08:12:43 +0100

    Pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaginaInicioComponent} from './pagina-inicio/pagina-inicio.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: PaginaInicioComponent}
+];
