717a26daad91600d30021cadd2bdacc8f6a25211
ene <eneritz.langoyo@insitutvidreres.cat>
2024-12-16 11:09:12 +0100

    arreglo de errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 import { FooterComponent} from './footer/footer.component';
-import { ProductosComponent} from './productos/productos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, ProductosComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { PaginaInicioComponent} from './pagina-inicio/pagina-inicio.component';
 import {PaginaFAQComponent } from './pagina-faq/pagina-faq.component';
 import { PaginaCondicionesComponent} from './pagina-condiciones/pagina-condiciones.component';
+import { PaginaCatalogoComponent } from './pagina-catalogo/pagina-catalogo.component';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
+import { PaginaRegisterComponent } from './pagina-register/pagina-register.component';
 
 
-// @ts-ignore
-// @ts-ignore
+
 export const routes: Routes = [
-  {path: '', component: PaginaInicioComponent, PaginaFAQComponent, PaginaCondicionesComponent}
+  {path: '', component: PaginaInicioComponent},
+  {path: 'faq', component: PaginaFAQComponent},
+  {path: 'condiciones', component: PaginaCondicionesComponent},
+  {path: 'catalogo', component: PaginaCatalogoComponent},
+  {path: 'registro', component: PaginaRegisterComponent},
+  {path: 'login', component: PaginaLoginComponent},
 ];
