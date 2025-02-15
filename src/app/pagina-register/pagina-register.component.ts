175658b2c347ea551550dbdfb39dde770fb9eca0
ene <eneritz.langoyo@insitutvidreres.cat>
2025-02-15 02:00:00 +0100

    Firebase register

--- src/app/pagina-register/pagina-register.component.ts
+++ src/app/pagina-register/pagina-register.component.ts
@@ -12,6 +12,7 @@ import {
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-pagina-register',
@@ -23,15 +24,10 @@ import { Router } from '@angular/router';
 })
 export class PaginaRegisterComponent {
   registerForm = new FormGroup({
-    nombreCompleto: new FormControl("", [Validators.required, Validators.minLength(4)]),
-    nombreUsuario: new FormControl("", [Validators.required, Validators.minLength(4)]),
     correoElectronico: new FormControl("", [Validators.email]),
-    numeroTelefono: new FormControl("", [Validators.required, Validators.minLength(4)]),
     contrasegna: new FormControl("", [Validators.required, Validators.minLength(8)]),
     confirmarContrasegna: new FormControl("", [Validators.required, Validators.minLength(8)]),
-    gender: new FormControl(""),
   }, { validators: this.passwdValidator });
-  user: User = new User();
 
   paginaNombre: string = 'GameStorm';
 
@@ -43,7 +39,7 @@ export class PaginaRegisterComponent {
     this.titleService.setTitle(`${this.paginaNombre} - Registro`);
   }
 
-  constructor(private router: Router, private titleService: Title) {};
+  constructor(private router: Router, private titleService: Title, public authService: AuthService) {};
 
 
   passwdValidator(): ValidatorFn {
@@ -56,99 +52,14 @@ export class PaginaRegisterComponent {
     };
   }
   onsubmit() {
-    console.log("Ñeh");
     if (this.registerForm.valid) {
       const formData = this.registerForm.value;
 
-      const newUser: User = {
-        nombre: formData.nombreCompleto || '',
-        userName: formData.nombreUsuario || '',
-        email: formData.correoElectronico || '',
-        phoneNumber: formData.numeroTelefono || '',
-        contrasegna: formData.contrasegna || '',
-        genero: formData.gender === 'Hombre' ? Genero.Hombre : Genero.Mujer,
-      };
-
-      UserStorage.addUser(newUser);
-
-      this.router.navigate(["login"]);
+      if(formData.correoElectronico && formData.contrasegna) {
+        this.authService.registerWithEmailAndPassword(formData.correoElectronico, formData.contrasegna)
+      }
+      this.router.navigate(["/login"]);
     }
   }
 }
-export class User {
-  nombre: string = "";
-  userName: string = "";
-  email: string = "";
-  phoneNumber: string = "";
-  contrasegna: string = "";
-  genero: Genero = Genero.Hombre;
-}
-export enum Genero {
-  Hombre,
-  Mujer,
-}
 
-export class UserStorage {
-  private static USER_STORAGE_NAME = "users";
-  private static LOGGED_STORAGE_NAME = "loggedIn";
-
-  static getMap(): Map<string, User> {
-    const storedMap = localStorage.getItem(this.USER_STORAGE_NAME);
-
-    if (!storedMap) {
-      return new Map();
-    }
-
-    const parsedObject = JSON.parse(storedMap);
-    return new Map(Object.entries(parsedObject));
-  }
-
-  static storeUsers(users: Map<string, User>): void {
-    const serializedMap = JSON.stringify(Object.fromEntries(users));
-    localStorage.setItem(this.USER_STORAGE_NAME, serializedMap);
-  }
-
-  static getUsers(): User[] {
-    const usersMap = this.getMap();
-    return Array.from(usersMap.values());
-  }
-
-  static getUser(userName: string): User | null {
-    const usersMap = this.getMap();
-    return usersMap.get(userName) || null;
-  }
-
-  static addUser(user: User): boolean {
-    const usersMap = this.getMap();
-
-    if (usersMap.has(user.userName)) {
-      return false;
-    }
-
-    usersMap.set(user.userName, user);
-    this.storeUsers(usersMap);
-    return true;
-  }
-
-  static userExists(userName: string): boolean {
-    const usersMap = this.getMap();
-    return usersMap.has(userName);
-  }
-
-  static loginUser(user: User): void {
-    localStorage.setItem(this.LOGGED_STORAGE_NAME, user.userName);
-  }
-
-  static getLoggedUserName(): string | null {
-    return localStorage.getItem(this.LOGGED_STORAGE_NAME);
-  }
-
-  static getLoggedUser(): User | null {
-    const userName = this.getLoggedUserName();
-    return userName ? this.getUser(userName) : null;
-  }
-
-  static logoutUser(): void {
-    localStorage.removeItem(this.LOGGED_STORAGE_NAME);
-  }
-}
